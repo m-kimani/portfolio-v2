8ca1b217f115e1f547c3bad55bdd45334060a993
Kimani Muhammad <kimanimuhammad@gmail.com>
2025-05-06 06:35:20 -0700

    move index

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default {
-  root: './', // Ensure root is set to the correct folder
+export default defineConfig({
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Make sure the path is correct
+    },
   },
-}
+});
