8f0645f2040feb191e0f61f2d246573aa780190a
Kimani Muhammad <kimanimuhammad@gmail.com>
2025-06-25 18:29:47 -0700

    Rearranged App routes to the correct order.

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,26 @@ const App = () => (
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
-        </div>
-      </div>
+  
       
       <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Routes>
+            <Route path="/" element={
+              <>
+                <Hero />
+                <Testimonials />
+                <Clients />
+                <CTA />
+              </>
+            } />
             <Route path="/projects" element={<Projects />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<div className="text-white p-10 text-center">Page Not Found</div>} />
           </Routes>
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Contact />
+
           <Footer />
           <BottomNav />
         </div>
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <a href="https://www.linkedin.com/in/kimani-muhammad-engineer/" target="_blank" rel="noopener noreferrer"> {/* Add your target URL here */}
+  <a href="https://www.linkedin.com/in/kimani-muhammad-engineer/" target="_blank" rel="noopener noreferrer"> {/* Link to LinkedIn Profile */ }
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
