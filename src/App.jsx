7538ab72cd7627e9e2e51fec08cd2520d9972314
Kimani Muhammad <kimanimuhammad@gmail.com>
2025-06-25 17:54:28 -0700

    Update page layout to use Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./style";
-import { Projects, Business, Blog, Resume, Contact, Clients, CTA, Footer, Navbar, Testimonials, Hero, MovingLine } from "./components";
+import { Projects, Blog, Resume, Contact, Clients, CTA, Footer, Navbar, Testimonials, Hero, MovingLine } from "./components";
 import BottomNav from "./components/BottomNav";
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <MovingLine />
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+  <Router>
+    <div className="bg-primary w-full overflow-hidden">
+      <MovingLine />
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Routes>
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Contact />
+          <Footer />
+          <BottomNav />
+        </div>
       </div>
     </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Resume />
-        <Projects />
-        <Testimonials />
-        <Clients />
-        <Business />
-        <Blog />
-        <CTA />
-        <Contact />
-        <Footer />
-        <BottomNav />
-      </div>
-    </div>
-  </div>
+  </Router>
 );
 
 export default App;
